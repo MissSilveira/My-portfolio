07e77c3ce7ba39e55451ed05d84d5274e31ddbc9
Eufémia Silveira <eufemiasilveira@hotmail.com>
2023-08-23 13:33:00 -0100

    atualização de conteudo

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ function typeWriter(elemento){
     //fazemos com que fique com NADA porque queremos que cada letra entre
     elemento.innerHTML = '';
     textoArray.forEach((letra, i) => {
-        //com isso podemos dizer "Adicione isso aqui em 100 segundos e esse proximo item em 200segundos"
         setTimeout(() => {
             elemento.innerHTML += letra;
         }, 75 * i)
@@ -13,7 +12,6 @@ function typeWriter(elemento){
 const titulo = document.getElementById('need-text');
 typeWriter(titulo);
 
-//typeWriter(document.querySelector('title'))
 
 
 const target = document.querySelectorAll('[data-anime]')
@@ -93,10 +91,34 @@ const projects = [
 
     {
         'name': 'Website-Clone--- Only Quality Food',
-        'urlImg': 'img/project-food.png',
+        'urlImg': 'img/project-only.png',
         'urlGithub': 'https://github.com/MissSilveira/Website-Clone---onlyQualityFood',
         'description': 'Im excited to present my HTML and CSS template clone, a project Ive crafted to enhance my web development competencies. This endeavor serves as a training ground where Ive replicated a carefully designed template using HTML and CSS, allowing me to sharpen my abilities and refine my understanding of modern web design principles.',
         'url': 'https://misssilveira.github.io/Website-Clone---onlyQualityFood/'
+    },
+
+    {
+        'name': 'Review: Four Seasons Seychelles',
+        'urlImg': 'img/project-review.png',
+        'urlGithub': 'https://github.com/MissSilveira/Responsive-Cloning-Reproduction-of-Review-Four-Seasons-Seychelles-Website',
+        'description': 'Responsive recreation of Review: Four Seasons Seychelles website using HTML, CSS & JS/jQuery. Faithfully translated template with interactivity. Adapts seamlessly to diverse screen sizes.',
+        'url': 'https://misssilveira.github.io/Responsive-Cloning-Reproduction-of-Review-Four-Seasons-Seychelles-Website/'
+    },
+
+    {
+        'name': 'Jaime consultancy website',
+        'urlImg': 'img/project-jaime.png',
+        'urlGithub': 'https://github.com/MissSilveira/Jaime-Consultancy-Website-Built-with-HTML-CSS-JavaScript-and-jQuery',
+        'description': 'This responsive site for Jaime Consultancy showcases technical skills with HTML, CSS, JS, and jQuery. It features animations, arrays, and Flexbox layouts',
+        'url': 'https://misssilveira.github.io/Jaime-Consultancy-Website-Built-with-HTML-CSS-JavaScript-and-jQuery/'
+    },
+
+    {
+        'name': 'NOMA | AUTHENTIC - HTML, CSS Grid page',
+        'urlImg': 'img/project-noma.png',
+        'urlGithub': 'https://github.com/MissSilveira/HTML-CSS-Grid-Page',
+        'description': 'Single-page grid layout crafted in HTML & CSS, mirroring Noma Authentic. Efficiently designed for responsive viewing across various devices',
+        'url': 'https://misssilveira.github.io/HTML-CSS-Grid-Page/'
     },
 
 ]
@@ -132,32 +154,3 @@ document.getElementById('menu')
 
 
 
-/*
-Welcome to my portfolio! As a front-end web developer, I am well-versed in a diverse range of technologies and tools, and I am excited to showcase my skills to you.
-
-**HTML, CSS, and JavaScript:**
-These three form the core foundation of my front-end development expertise. I craft clean and semantic HTML structures, style them with CSS to create visually appealing designs, and bring life to web applications through interactive and dynamic functionality using JavaScript.
-
-**JSON:**
-I am proficient in working with JSON (JavaScript Object Notation), which plays a crucial role in handling data interchange between the client and server. Leveraging JSON allows me to build efficient and scalable applications.
-
-**PHP and MySQL:**
-My skill set extends to server-side development with PHP, a powerful scripting language for creating robust back-end logic. Combined with MySQL, I can effectively manage databases and implement dynamic data-driven solutions.
-
-**Bootstrap and jQuery:**
-I harness the power of Bootstrap to develop responsive, mobile-first web applications that offer an exceptional user experience across various devices and screen sizes. Additionally, jQuery empowers me to simplify complex tasks and create visually appealing animations and effects.
-
-**TypeScript:**
-I have expanded my toolkit to include TypeScript, a superset of JavaScript that adds static typing to the language. TypeScript enables me to catch potential errors during development, leading to more reliable code and improved collaboration.
-
-In my portfolio, you will find a curated selection of projects that demonstrate my expertise in these technologies. From captivating user interfaces to seamless interactions and database-driven applications, each project showcases the dedication and passion I invest in my work.
-
-I am committed to staying at the forefront of the ever-evolving web development landscape. As new technologies emerge, I eagerly embrace the opportunity to enhance my skills and tackle exciting challenges.
-
-Thank you for taking the time to explore my portfolio. If you have any inquiries, potential collaborations, or projects that could benefit from my expertise, please feel free to get in touch. Let's build the future of the web together!
-
-Best regards,
-[Your Name]
-
-
-*/
